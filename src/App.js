51196f4211539c1da71740830639f019457f1562
s-t-e-v <111192733+s-t-e-v@users.noreply.github.com>
2023-05-09 21:23:53 +0200

    main structure + css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 
 function App() {
   return (
-   <>
-    Hello world!
-   </>
+   <div className='App'>
+
+      <h1 className='appTitle'>Project 4: Shopping List</h1>
+
+      <main className='todo-container'>
+
+        <h2>Items To Buy</h2>
+
+        <form>
+
+            <div className='entry-container'>
+              <input type="text" placeholder='Add a new item' />
+              <button>Add</button>
+            </div>
+
+        </form>
+      </main>
+    
+   </div>
   );
 }
 
